604447e8c00f6c534dba8bbf3fc7c3bd46738c23
Felipe Cruz <felipephplow@gmail.com>
2022-08-09 17:00:30 -0300

    s

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -11,19 +11,20 @@ class CartController extends Controller
     public function cartAdd(Request $request)
     {
 
-        $cartItens = $request->all();
+        $cartItens = collect($request->all());
         \Log::info($cartItens);
-        foreach ($cartItens as $key => $iten) {
-            $cart = CartItem::create([
-                'customer_id' => $iten['customer_id'],
-                'seller_id' => $iten['seller_id'],
-                'brand' => $iten['brand'],
-                'type' => $iten['type'],
-                'price' => $iten['price'],
-                'product_type' => $iten['product_type'],
-                'quantity' => $iten['quantity'],
-            ]);
-        }
+        $cart = CartItem::create($cartItens->toArray());
+        // foreach ($cartItens as $key => $iten) {
+        //     $cart = CartItem::create([
+        //         'customer_id' => $iten['customer_id'],
+        //         'seller_id' => $iten['seller_id'],
+        //         'brand' => $iten['brand'],
+        //         'type' => $iten['type'],
+        //         'price' => $iten['price'],
+        //         'product_type' => $iten['product_type'],
+        //         'quantity' => $iten['quantity'],
+        //     ]);
+        // }
         \Log::info($cart);
         return response()->json('success', 200);
     }
